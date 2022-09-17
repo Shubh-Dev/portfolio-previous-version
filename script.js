bff6ebe47be4392401224a1119897c4c61b658d4
Shubh <shubh.scb@gmail.com>
2022-09-17 17:18:04 +0530

    REFACTOR MOBILER VERSON IN PROGRESS

--- script.js
+++ script.js
@@ -83,33 +83,40 @@ projects.map((item) => {
   return document.querySelector('.project-section').appendChild(project);
 });
 
-// const popMenu = document.querySelector('.hamburger');
-// popMenu.addEventListener('click', () => {
-//   const myHeader = document.querySelector('.main-header');
-//   myHeader.classList.add('active');
-//   const myNavbar = document.querySelector('.main-nav');
-//   myNavbar.classList.add('active');
-//   const myLogo = document.querySelector('.my-logo');
-//   myLogo.classList.add('active');
-//   const lineBar = document.querySelector('.hamburger');
-//   lineBar.classList.add('active');
-//   const imgCross = document.querySelector('.cross-img');
-//   imgCross.classList.add('active');
-// });
 
-// const popOutMenu = document.querySelector('.cross-img');
+const popMenu = document.querySelector('.hamburger');
+popMenu.addEventListener('click', () => {
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.add('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.add('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.add('active');
+  const lineBar = document.querySelector('.hamburger');
+  lineBar.classList.add('active');
+  const imgCross = document.querySelector('.cross-img');
+  imgCross.classList.add('active');
+});
+
+const popOutMenu = document.querySelector('.cross-img');
+popOutMenu.addEventListener('click', () => {
+  const myHeader = document.querySelector('.main-header');
+  myHeader.classList.remove('active');
+  const myNavbar = document.querySelector('.main-nav');
+  myNavbar.classList.remove('active');
+  const myLogo = document.querySelector('.my-logo');
+  myLogo.classList.remove('active');
+  const lineBar = document.querySelector('.hamburger');
+  lineBar.classList.remove('active');
+  const imgCross = document.querySelector('.cross-img');
+  imgCross.classList.remove('active');
+});
+
+// const popOutMenu = document.querySelector('.cross-img')
 // popOutMenu.addEventListener('click', () => {
-//   const myHeader = document.querySelector('.main-header');
-//   myHeader.classList.remove('active');
-//   const myNavbar = document.querySelector('.main-nav');
-//   myNavbar.classList.remove('active');
-//   const myLogo = document.querySelector('.my-logo');
-//   myLogo.classList.remove('active');
-//   const lineBar = document.querySelector('.hamburger');
-//   lineBar.classList.remove('active');
-//   const imgCross = document.querySelector('.cross-img');
-//   imgCross.classList.remove('active');
-// });
+
+// }
+
 
 // const popOutMenu2 = document.querySelector('.link-3');
 // popOutMenu2.addEventListener('click', () => {
